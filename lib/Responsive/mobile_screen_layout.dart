c7fcd89a7863dd02d1b4bf2949c326f24e78d001
sourav kumar pandey <soukumpan@gmail.com>
2023-04-20 20:20:00 +0530

    adding bottom bar

--- lib/Responsive/mobile_screen_layout.dart
+++ lib/Responsive/mobile_screen_layout.dart
@@ -3,8 +3,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/providers/user_provider.dart';
+import 'package:instagram_clone/utils/colors.dart';
 import 'package:provider/provider.dart';
-import 'package:instagram_clone/models/user.dart'as model;
+import 'package:instagram_clone/models/user.dart' as model;
 
 // class MobileScreen extends StatefulWidget {
 //   const MobileScreen({super.key});
@@ -27,7 +28,7 @@ import 'package:instagram_clone/models/user.dart'as model;
 //         .collection('users')
 //         .doc(FirebaseAuth.instance.currentUser!.uid)
 //         .get();
-//     print(snap.data()) ;   
+//     print(snap.data()) ;
 //     setState(() {
 //       username=(snap.data()as Map<String,dynamic>)['username'];
 //     });
@@ -42,25 +43,107 @@ import 'package:instagram_clone/models/user.dart'as model;
 //     );
 //   }
 // }
-class MobileScreen extends StatelessWidget {
+class MobileScreen extends StatefulWidget {
   const MobileScreen({super.key});
 
   @override
+  State<MobileScreen> createState() => _MobileScreenState();
+}
+
+class _MobileScreenState extends State<MobileScreen> {
+  @override
+  int _page = 0;
+  late PageController pageController=PageController();
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    super.initState();
+    pageController=PageController();
+  }
+  void onPageChanged(int page){
+    setState(() {
+      _page=page;
+    });
+  }
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+  void navigationTapped(int page){
+pageController.jumpToPage(page);
+  }
   Widget build(BuildContext context) {
-    model.User? user= Provider.of<UserProvider>(context).getUser;
-    return user == null
+//     model.User? user= Provider.of<UserProvider>(context).getUser;
+//     return user == null
 
-? const Center(
+// ? const Center(
 
-child: CircularProgressIndicator(
+// child: CircularProgressIndicator(
 
-),)
+// ),)
 
-: 
+// :
 
-  Scaffold(
-      body: Center(
-        child: Text(user.email ),
+    return Scaffold(
+      body: PageView(
+        children: [
+        Text('feed'),
+        Text('search'),
+        Text('add post'),
+        Text('notification'),
+        Text('profile'),
+        ],
+        physics:const  NeverScrollableScrollPhysics(),
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        ),
+      bottomNavigationBar: BottomNavigationBar(
+     
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: _page == 0 ? primaryColor : secondaryColor,
+            ),
+            label: '',
+             backgroundColor: mobileBackgroundColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              color: _page == 1 ? primaryColor : secondaryColor,
+            ),
+            label: '',
+           
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add_circle,
+              color: _page == 2 ? primaryColor : secondaryColor,
+            ),
+            label: '',
+           
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite,
+              color: _page == 3 ? primaryColor : secondaryColor,
+            ),
+            label: '',
+         
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+              color: _page == 4 ? primaryColor : secondaryColor,
+            ),
+            label: '',
+            
+          ),
+        ],
+        onTap: navigationTapped,
       ),
     );
   }
