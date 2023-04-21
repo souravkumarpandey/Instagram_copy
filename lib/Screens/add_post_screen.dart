265ad95013d88afa1c1e4f1ede9c109b72beb47a
sourav kumar pandey <soukumpan@gmail.com>
2023-04-21 07:03:19 +0530

    selection of image is done

--- lib/Screens/add_post_screen.dart
+++ lib/Screens/add_post_screen.dart
@@ -1,5 +1,12 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:instagram_clone/providers/user_provider.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/utils.dart';
+import 'package:provider/provider.dart';
+import 'package:instagram_clone/models/user.dart' as model;
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -9,17 +16,58 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
+  final TextEditingController _descriptionController= TextEditingController();
+  Uint8List? _file;
   @override
-  Widget build(BuildContext context) {
-    // return Center(
-    //   child: IconButton(
-    //     icon: const Icon(Icons.upload),
-    //     onPressed: () {
+  _selectImage(BuildContext context)async{
+    return showDialog(context: context, builder: (context){
+      return SimpleDialog(
+        title: const Text('Create a Post'),
+        children: [
+          SimpleDialogOption(
+            padding: const EdgeInsets.all(20),
+            child: const Text('Take a photo'),
+            onPressed: ()async{
+              Navigator.of(context).pop();
+              Uint8List file= await pickImage(ImageSource.camera);
+              setState(() {
+                _file=file;
+              });
+            },
+          ),
+          SimpleDialogOption(
+            padding: const EdgeInsets.all(20),
+            child: const Text('Choose from Gallery'),
+            onPressed: ()async{
+              Navigator.of(context).pop();
+              Uint8List file= await pickImage(ImageSource.gallery);
+              setState(() {
+                _file=file;
+              });
+            },
+          ),
+          SimpleDialogOption(
+            padding: const EdgeInsets.all(20),
+            child: const Text('Cancel'),
+            onPressed: (){
+              Navigator.of(context).pop();
+            
+            },
+          ),
+        ],
+      );
 
-    //     },
-    //   ),
-    // );
-    return Scaffold(
+    });
+  }
+  Widget build(BuildContext context) {
+    model.User? user= Provider.of<UserProvider>(context).getUser;
+    return _file==null? Center(
+      child: IconButton(
+        icon: const Icon(Icons.upload),
+        onPressed: ()=>_selectImage(context),
+      ),
+    ):
+     Scaffold(
       appBar: AppBar(
         backgroundColor: mobileBackgroundColor,
         leading: IconButton(
@@ -50,11 +98,12 @@ class _AddPostScreenState extends State<AddPostScreen> {
             children: [
               CircleAvatar(
                 backgroundImage: NetworkImage(
-                    'https://images.unsplash.com/photo-1536152470836-b943b246224c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'),
+                    user!.photoUrl),
               ),
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.45,
                 child: TextField(
+                  controller: _descriptionController,
                   decoration: const InputDecoration(
                     hintText: 'Write a caption',
                     border: InputBorder.none,
@@ -70,7 +119,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
                   child: Container(
                     decoration: BoxDecoration(
                       image: DecorationImage(
-                        image: NetworkImage('https://images.unsplash.com/photo-1523712999610-f77fbcfc3843?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'),
+                        image: MemoryImage(_file!),
                         fit: BoxFit.fill,
                         alignment:FractionalOffset.topCenter,
 
